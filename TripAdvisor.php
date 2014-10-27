4d8a500fd8ed1e2f96bc674b04b864a7c6233d99
Phil Poore <phil@byte22.com>
2014-10-27 15:24:20 +0000

    Update TripAdvisor.php

--- TripAdvisor.php
+++ TripAdvisor.php
@@ -25,10 +25,10 @@ class TripAdvisor {
 		$now = time();
 		$file_mtime_offset = $now - $file_mtime;
 		if ($file_mtime_offset > $this->CACHE_UPDATE_INTERVAL){
-			file_put_contents(dirname(__FILE__) ."TripAdvisorAPI.log", "[$now] Updating $filename because its $file_mtime_offset seconds old.", FILE_APPEND);
+			file_put_contents(dirname(__FILE__) ."/TripAdvisorAPI.log", "[$now] Updating $filename because its $file_mtime_offset seconds old.", FILE_APPEND);
 			$this->update($location_id);
 		}else{
-			file_put_contents(dirname(__FILE__) ."TripAdvisorAPI.log", "[$now] Skipping Update because its $file_mtime_offset seconds old.", FILE_APPEND);
+			file_put_contents(dirname(__FILE__) ."/TripAdvisorAPI.log", "[$now] Skipping Update because its $file_mtime_offset seconds old.", FILE_APPEND);
 		}
 	}
 	
